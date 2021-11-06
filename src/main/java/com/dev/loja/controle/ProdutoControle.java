0825997e512b7c06a3319a712c0cab8bf8484099
angelMizutani <angelisayuri@yahoo.com.br>
2021-11-06 18:32:08 -0300

    inserção de produtos em lote e paginação

--- src/main/java/com/dev/loja/controle/ProdutoControle.java
+++ src/main/java/com/dev/loja/controle/ProdutoControle.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,9 +30,17 @@ import com.dev.loja.modelos.Produto;
 import com.dev.loja.repositorios.CategoriaRepositorio;
 import com.dev.loja.repositorios.MarcaRepositorio;
 import com.dev.loja.repositorios.ProdutoRepositorio;
+import com.dev.loja.servico.ProdutoServico;
 
 @Controller
 public class ProdutoControle {
+	
+	private final ProdutoServico produtoServico;
+
+	@Autowired
+	public ProdutoControle(ProdutoServico produtoServico){
+		this.produtoServico = produtoServico;
+	}
 	
 	private static String caminhoImagens = "C:\\Users\\angel\\OneDrive\\Documentos\\Engenharia de Software\\3o. Ano\\Programação WEB\\imagens_loja\\";
 	
@@ -42,6 +52,8 @@ public class ProdutoControle {
 
 	@Autowired
 	private MarcaRepositorio marcaRepositorio;
+
+	
 	
 	@GetMapping("/administrativo/entrada/produtos/cadastrar")
 	public ModelAndView cadastrar(Produto produto) {
@@ -50,6 +62,18 @@ public class ProdutoControle {
 		mv.addObject("listaCategorias", categoriaRepositorio.findAll());
 		mv.addObject("listaMarcas", marcaRepositorio.findAll());
 		return mv;
+	}
+
+	@GetMapping("/administrativo/entrada/produtos/listarPag")
+	public ModelAndView listarPag(
+		@RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber, 
+		@RequestParam(value = "size", required = false, defaultValue = "5") int size) {
+
+		ModelAndView mv = new ModelAndView("/administrativo/produtos/lista");
+		mv.addObject("listaProdutos", produtoServico.getPage(pageNumber, size));
+
+		return mv;
+
 	}
 	
 	@GetMapping("/administrativo/entrada/produtos/listar")
@@ -60,25 +84,34 @@ public class ProdutoControle {
 	}
 
 	@PostMapping("/administrativo/produtos/buscarDescricao")
-	public ModelAndView buscarPorDescricao(@RequestParam("buscar_descricao") String descricao){
+	public ModelAndView buscarPorDescricao(
+		@RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber, 
+		@RequestParam(value = "size", required = false, defaultValue = "5") int size,
+		@RequestParam("buscar_descricao") String descricao){
 		ModelAndView mv = new ModelAndView("/administrativo/produtos/lista");
-		mv.addObject("listaProdutos", produtoRepositorio.findByDescricaoContaining(descricao));
+		mv.addObject("listaProdutos", produtoServico.getPageDescricao(pageNumber, size, descricao));
 		return mv;
 	}
 
 	@PostMapping("/administrativo/produtos/buscarCategoria")
-	public ModelAndView buscarPorCategoria(@RequestParam("buscar_categoria") String categoria){
+	public ModelAndView buscarPorCategoria(
+		@RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber, 
+		@RequestParam(value = "size", required = false, defaultValue = "5") int size,
+		@RequestParam("buscar_categoria") String categoria){
 		ModelAndView mv = new ModelAndView("/administrativo/produtos/lista");
 		Optional<Categoria> opCategoria = categoriaRepositorio.findByNomeContaining(categoria);
-		mv.addObject("listaProdutos", produtoRepositorio.findByCategoria(opCategoria));
+		mv.addObject("listaProdutos", produtoServico.getPageCategoria(pageNumber, size, opCategoria));
 		return mv;
 	}
 
 	@PostMapping("/administrativo/produtos/buscarMarca")
-	public ModelAndView buscarPorMarca(@RequestParam("buscar_marca") String marca){
+	public ModelAndView buscarPorMarca(
+		@RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber, 
+		@RequestParam(value = "size", required = false, defaultValue = "5") int size,
+		@RequestParam("buscar_marca") String marca){
 		ModelAndView mv = new ModelAndView("/administrativo/produtos/lista");
 		Optional<Marca> opMarca = marcaRepositorio.findByNomeContaining(marca);
-		mv.addObject("listaProdutos", produtoRepositorio.findByMarca(opMarca));
+		mv.addObject("listaProdutos", produtoServico.getPageMarca(pageNumber, size, opMarca));
 		return mv;
 	}
 	
@@ -127,6 +160,30 @@ public class ProdutoControle {
 		}
 		
 		
+		return cadastrar(new Produto());
+	}
+
+	@PostMapping(value="/administrativo/entrada/produtos/salvar", params = "salvar_lotes")
+	public ModelAndView salvarEmLotes() {
+
+		List<Produto> lote = new ArrayList<>();
+		Optional<Categoria> op_categoria = categoriaRepositorio.findById(70L);
+		Categoria categoria = op_categoria.get();
+
+		Optional<Marca> op_marca = marcaRepositorio.findById(75L);
+		Marca marca = op_marca.get();
+
+		for (int i = 0; i < 50000; i++){
+			Produto produto = new Produto();
+			produto.setDescricao("teste");
+			produto.setCategoria(categoria);
+			produto.setMarca(marca);
+			produto.setValorVenda(10.0);
+			lote.add(produto);
+		}
+
+		produtoRepositorio.saveAllAndFlush(lote);
+
 		return cadastrar(new Produto());
 	}
 
--- src/main/java/com/dev/loja/repositorios/ProdutoRepositorio.java
+++ src/main/java/com/dev/loja/repositorios/ProdutoRepositorio.java
@@ -1,5 +1,7 @@
 package com.dev.loja.repositorios;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,7 +12,7 @@ import com.dev.loja.modelos.Marca;
 import com.dev.loja.modelos.Produto;
 
 public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
-    List<Produto> findByDescricaoContaining(String descricao);
-    List<Produto> findByCategoria(Optional<Categoria> categoria);  
-    List<Produto> findByMarca(Optional<Marca> marca); 
+    Page<Produto> findByDescricaoContaining(Pageable pageable, String descricao);
+    Page<Produto> findByCategoria(Pageable pageable, Optional<Categoria> categoria);  
+    Page<Produto> findByMarca(Pageable pageable, Optional<Marca> marca); 
 }
