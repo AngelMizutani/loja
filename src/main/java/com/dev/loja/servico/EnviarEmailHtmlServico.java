c969806aa94fa06764531bbe925e2c3edf120882
angelMizutani <angelisayuri@yahoo.com.br>
2021-11-07 22:01:18 -0300

    Retirado campo de senha no cadastro e envio de email para cadastrar senha

--- src/main/java/com/dev/loja/servico/EnviarEmailHtmlServico.java
+++ src/main/java/com/dev/loja/servico/EnviarEmailHtmlServico.java
@@ -3,12 +3,9 @@ package com.dev.loja.servico;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Service
 public class EnviarEmailHtmlServico {
